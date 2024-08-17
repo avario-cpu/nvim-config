bf7931917e129e7b59ff8f0a7d2ca4664e7619be
Loïc Villet <villet.loic@gmail.com>
2024-08-17 16:10:58 +0200

    Add test text for merge

--- init.lua
+++ init.lua
@@ -4,3 +4,22 @@ require("config.keybindings")
 require("config.harpoon_config")
 require("autocommands.general_autocmds")
 require("autocommands.no_neck_pain_autocmds")
+
+-- My first comment for merge (do not keep this one)
+-- My second comment (keep this one)
+-- Third: (do not keep)
+
+-- pick this
+-- and this
+-- and this
+
+-- Here,
+-- Back to manual mode
+-- And we then ...
+
+-- Here
+-- back to premade
+-- back to premade
+
+-- some premade again
+-- pick this over main
