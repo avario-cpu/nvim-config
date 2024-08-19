d45e1547b71985685c4cb6857de995d4a7a8d47f
Loïc Villet <villet.loic@gmail.com>
2024-08-19 18:16:49 +0200

    Adjust green value for rainbow delimiters

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -2,13 +2,17 @@ return {
   "HiPhish/rainbow-delimiters.nvim",
   event = "BufReadPost",
   config = function()
+    -- Slightly brighter green
+    vim.api.nvim_set_hl(0, "RainbowDelimiterEnhancedGreen", { fg = "#739c84" })
+
     require("rainbow-delimiters.setup").setup({
       highlight = {
         "RainbowDelimiterRed",
         "RainbowDelimiterYellow",
         "RainbowDelimiterBlue",
         "RainbowDelimiterOrange",
-        "RainbowDelimiterGreen",
+        -- "RainbowDelimiterGreen",
+        "RainbowDelimiterEnhancedGreen", -- Our slightly brighter green
         "RainbowDelimiterViolet",
         "RainbowDelimiterCyan",
       },
