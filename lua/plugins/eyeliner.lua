4cb6dc28904ec60e337d23a3e49bdb303747bb4b
Loïc Villet <villet.loic@gmail.com>
2024-08-20 19:56:21 +0200

    Toggles

--- lua/plugins/eyeliner.lua
+++ lua/plugins/eyeliner.lua
@@ -1,9 +1,10 @@
 return {
   "jinh0/eyeliner.nvim",
+  enabled = false,
   config = function()
     require("eyeliner").setup({
-      -- highlight_on_key = true, -- this must be set to true for dimming to work!
-      -- dim = true,
+      highlight_on_key = true, -- this must be set to true for dimming to work!
+      dim = true,
     })
   end,
 }
