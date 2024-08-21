7f7afcb3a13218f61a76b57ab3434a6b4967002a
Loïc Villet <villet.loic@gmail.com>
2024-08-21 18:55:27 +0200

    Make lualine show more stuff

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,16 +4,60 @@ return {
   config = function()
     require("lualine").setup({
       options = {
+        icons_enabled = true,
         theme = "auto",
+        -- component_separators = { left = "", right = "" },
+        -- section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = true,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        },
       },
       sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
         lualine_x = {
           {
             require("noice").api.statusline.mode.get,
             cond = require("noice").api.statusline.mode.has,
             color = { fg = "#ff9e64" },
           },
+          "encoding",
+          "fileformat",
+          "filetype",
         },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {
+        "fugitive",
+        "fzf",
+        "lazy",
+        "mason",
+        "neo-tree",
+        "quickfix",
+        "trouble",
+        "oil",
       },
     })
   end,
